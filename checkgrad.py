Fix relative error computation

checkgrad.py
@@ -81,7 +81,7 @@ class GradientError(Exception):
 
 
 def compare_grad(grad, approx_grad):
-    err = np.linalg.norm(grad - approx_grad / approx_grad)
+    err = np.linalg.norm(grad - approx_grad) / np.linalg.norm(approx_grad)
     if err > 1e-5:
         raise GradientError(grad, approx_grad, err)
 